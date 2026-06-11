c80036ae3285de1f64470e83b6c9e3279f9769c2
Ankit Kumar <ankitkumar.136118@marwadiuniversity.ac.in>
2026-06-11 12:45:54 +0530

    Class-1
    Practical

---
+++ readexcelandcsv.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+winedatac=pd.read_csv('wine.csv')
+
+print(winedatac)
+print(winedatac.head())
+print("shape\n",winedatac.shape)
+print("columns\n",winedatac.columns)
+print("dtypes\n",winedatac.dtypes)
+print("ndim\n",winedatac.ndim)
+print("size\n",winedatac.size)
+
+
+winedatae=pd.read_excel('wine.xlsx')
+
+print('\n')
+
+
